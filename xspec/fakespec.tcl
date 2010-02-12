fcc55d672bf9d72ccc26d47374bdc1a08c831dca
Andreas Skielboe <skielboe@gmail.com>
2010-02-12 17:00:21 +0100

    Added TCL code to generate fake spectra with varying temperature. Included response files.

---
+++ xspec/fakespec.tcl
@@ -0,0 +1,47 @@
+proc fakespec { args } {
+	
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# BEGIN > DEFINITIONS - CHANGE THESE TO YOUR SPECIFIC TASK! #
+
+	# Filenames
+	set file_response "1666_3.wrmf"
+	set file_arf "1666_3.warf"
+	
+	# Model Parameters
+	set nspectra 10.
+	set temp_min 1.
+	set temp_max 5.
+	set nH 1.
+	set abundance 0.3
+	set redshift 0.18
+	
+	# Fakeit Parameters
+	set exposuretime 41796.2
+	
+# END > DEFINITIONS                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+	# set i 0
+	# set temp $temp_min
+	set stepsize [expr ($temp_max-$temp_min)/$nspectra]
+
+	# Initiate the model (ignoring parameters for now)
+	# syntax: model mekal & $temperature & $nH & $abundance & $redshift & & $norm &
+	model mekal & & & & & & &
+	
+	# Load response files
+	# response $file_response
+	# arf $file_arf
+	
+	
+	
+	# Run through all parameters generating spectra and dumping to unique ACSII files
+	# Exposure time, correction norm (41796.2, 0.00000)
+	for {set temp $temp_min} {$temp <= $temp_max} {set temp [expr $temp + $stepsize]} {
+		newpar 1 & $temp
+		puts "Faking spectrum with temp: $temp"
+		fakeit none & $file_response & $file_arf & y & & ./output/fakespec.fak & $exposuretime &
+		puts "Dumping spectrum to: fakespec_$temp.txt"
+		fdump infile=./output/fakespec.fak outfile=./output/fakespec_$temp.txt columns='COUNTS' rows=1-1024 prhead=no
+	}
+}
