c9a6bc9a009877000cb87a43aa1920fc5bd64da4
Andreas Skielboe <askielboe@askielboe.local>
2010-03-21 20:24:17 +0100

    Changed directory structure. Added parameters.dat for TCL and Fortran to communicate parameters. Added fortran code to generate all channels at once (many times faster than the old code). Its called profile_all.f90. Normalized the counts to exposure-time * 1000 in the fortran output.

--- xspec/fakespec.tcl
+++ xspec/fakespec.tcl
@@ -3,14 +3,14 @@ proc fakespec { args } {
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # BEGIN > DEFINITIONS - CHANGE THESE TO YOUR SPECIFIC TASK! #
 
-	# Filenames
-# 	set file_response "1666_3.wrmf"
-# 	set file_arf "1666_3.warf"
+	# Uncomment next line to use external response matrices
+	# set file_response "1666_3.wrmf"
+	# set file_arf "1666_3.warf"
 	
 	# Parameter to vary
-	set ipar 6			; # 1: Temp, 2: nH, 3: Abundance, 4: redshift, 6: norm
-	set param_min 1.	; # Decimals are limited to 3 places,
-	set param_max 500.	; # this can be changed, but then has to be changed in the fortran code as well.
+	set ipar 1			; # 1: Temp, 2: nH, 3: Abundance, 4: redshift, 6: norm
+	set param_min 0.1	; # Decimals are limited to 3 places,
+	set param_max 15.	; # this can be changed, but then has to be changed in the fortran code as well.
 	set nspectra 100.	; # Note that we actually get nspectra+1 spectra!
 	
 	# Model Parameters
@@ -23,12 +23,12 @@ proc fakespec { args } {
 	set norm 1.
 	
 	# Fakeit Parameters
-	set exposuretime 41796.2
+	set exposuretime 4179600.2
 	
 # END > DEFINITIONS                                         #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-if {[exec ls ./output/] == ""} then {
+if {[exec ls ./data/output/] == ""} then {
 	# Note that this way we actually get nspectra+1 spectra!
 	set stepsize [expr ($param_max-$param_min)/$nspectra]
 	
@@ -37,19 +37,22 @@ if {[exec ls ./output/] == ""} then {
 	# Initiate the model (ignoring parameters for now)
 	model mekal & $temp & $nH & $abundance & $redshift & $switch & $norm &
 
-	
-
 	# Run through all parameters generating spectra and dumping to unique ACSII files
 	for {set param $param_min} {$param <= $param_max} {set param [expr $param + $stepsize]} {
 		newpar $ipar & $param
 		data none
 		puts "Faking spectrum with parameter $ipar = $param."
-		fakeit none & &y & & ./output/fakespec.fak & $exposuretime &
+		fakeit none & &y & & ./data/output/fakespec.fak & $exposuretime &
 		# Uncomment next line to use external response matrices
-		# fakeit none & $file_response & $file_arf & y & & ./output/fakespec.fak & $exposuretime &
+		# fakeit none & $file_response & $file_arf & y & & ./data/output/fakespec.fak & $exposuretime &
 		puts "Dumping spectrum to: fakespec_$ipar-$param.txt"
-		fdump infile=./output/fakespec.fak outfile=./output/fakespec_$ipar-[format "%4.3f" $param].txt columns='COUNTS' rows=1-1024 prhead=no
+		fdump infile=./data/output/fakespec.fak outfile=./data/output/fakespec_$ipar-[format "%4.3f" $param].txt columns='COUNTS' rows=1-1024 prhead=no
 		}
+	# Write log-file for Fortran code to use
+	set fout [open "./data/output/parameters.dat" w]
+	puts $fout "$ipar $param_min $param_max $exposuretime"
+	close $fout
+
 } else {
 	set question "N"
 	puts "OUTPUT DIRECTORY NOT EMPTY!"
@@ -57,7 +60,7 @@ if {[exec ls ./output/] == ""} then {
 	puts "If you want to delete all content of the output folder press 'y' now."
 	gets stdin question
 		if {$question == "y"} then {
-			rm ./output/*
+			rm ./data/output/*
 			puts "Deleting contents of output folder. Please run fakespec again to generate new spectra."
 		} else {
 			puts "Output directory NOT empty. Stopping..."
--- xspec/profile.f90
+++ xspec/profile.f90
@@ -11,13 +11,13 @@ INTEGER 			:: chosenchannel=300
 
 ! SET THE PARAMTER TO THE ONE YOU OF CHOISE
 ! (1: Temp, 2: nH, 3: Abundance, 4: redshift, 6: norm)
-INTEGER				:: ipar=6
+INTEGER				:: ipar
 
 ! SET THE NUMBER OF SPECTRA (this is nspectra from TCL script + 1)
 INTEGER,PARAMETER	:: nspectra=101
 
 ! SET THE MAX AND MIN VALUE OF CHOSEN PARAMETER
-REAL				:: param_min=1., param_max=500.
+REAL				:: param_min, param_max, exposure
 
 !-------------------------------------------------------------------
 
@@ -26,7 +26,14 @@ INTEGER,DIMENSION(nspectra,nchannels)	:: channel, count
 INTEGER									:: i,n
 REAL,DIMENSION(nspectra)				:: params
 REAL									:: param, stepsize
-CHARACTER 								:: dummy*7, fnamein*32, fnameout*32
+CHARACTER 								:: dummy*7, fnamein*50, fnameout*50
+
+!-------------------------------------------------------------------
+! Read in parameters from TCL logfile
+open(1,file='./data/output/parameters.dat', status='old', form='FORMATTED')
+read(1,*) ipar, param_min, param_max, exposure
+close(1)
+!-------------------------------------------------------------------
 
 ! Get command line arguments. If we get one, assume its the chosen channel and overwrite that variable.
 CALL GETARG(1,BUFFER)
@@ -41,13 +48,13 @@ end do
 
 do n = 1, nspectra
 	if (params(n) < 9.999) then
-		write(fnamein,'(a,I1,a,F5.3,a)') './output/fakespec_',ipar,'-',params(n),'.txt'
+		write(fnamein,'(a,I1,a,F5.3,a)') './data/output/fakespec_',ipar,'-',params(n),'.txt'
 	else if (params(n) < 99.999) then
-		write(fnamein,'(a,I1,a,F6.3,a)') './output/fakespec_',ipar,'-',params(n),'.txt'
+		write(fnamein,'(a,I1,a,F6.3,a)') './data/output/fakespec_',ipar,'-',params(n),'.txt'
 	else if (params(n) < 999.999) then
-		write(fnamein,'(a,I1,a,F7.3,a)') './output/fakespec_',ipar,'-',params(n),'.txt'
+		write(fnamein,'(a,I1,a,F7.3,a)') './data/output/fakespec_',ipar,'-',params(n),'.txt'
 	else
-		write(fnamein,'(a,I1,a,F8.3,a)') './output/fakespec_',ipar,'-',params(n),'.txt'
+		write(fnamein,'(a,I1,a,F8.3,a)') './data/output/fakespec_',ipar,'-',params(n),'.txt'
 	end if
 	!print *, 'Processing file: ', fnamein
 	open(1,file=fnamein,form='formatted')
@@ -59,16 +66,16 @@ end do
 close(1)
 
 ! Print output to file (2-d)
-write(fnameout,'(a,I1,a,I5,a)') './output/profile_',ipar,'_',chosenchannel,'.txt'
+write(fnameout,'(a,I1,a,I5,a)') './data/profiles/profile_',ipar,'_',chosenchannel,'.txt'
 open(2,file=fnameout, status="replace", form='FORMATTED')
 
 do n = 1,nspectra
-	write(2,'(F7.3,a,I10)') params(n), ' ', count(n,chosenchannel)
+	write(2,'(F7.3,a,F10.7)') params(n), ' ', count(n,chosenchannel)/exposure*1000
 end do
 close(2)
 
 ! Print output to file (3-d)
-! open(2,file='./output/profile.txt', status="replace", form='FORMATTED')
+! open(2,file='./data/profiles/profile.txt', status="replace", form='FORMATTED')
 ! 
 ! do i = 1, 300/3 ! Loop over channels.
 ! 	do n = 1,nspectra ! Loop over parameters
@@ -78,6 +85,6 @@ close(2)
 ! close(2)
 
 write(*,*) 'DONE PROCESSING FILES!'
-write(*,'(a,I1,a,I5,a)') 'Output written to: ./output/profile_',ipar,'_',chosenchannel,'.txt'
+write(*,'(a,I1,a,I5,a)') 'Output written to: ./data/profiles/profile_',ipar,'_',chosenchannel,'.txt'
 
 end program profile
